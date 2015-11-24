0077641061f5976cfa763072daf685684d3bf3f2
jecamargo10 <javier_22ca@hotmailcom>
2015-11-24 01:30:26 -0500

    Mobil
    asd

--- Sonar/persistencia/Arquisoft/target/classes/com/example/models/Emergencia_.java
+++ Sonar/persistencia/Arquisoft/target/classes/com/example/models/Emergencia_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-08T20:11:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-23T22:28:39")
 @StaticMetamodel(Emergencia.class)
 public class Emergencia_ { 
 
